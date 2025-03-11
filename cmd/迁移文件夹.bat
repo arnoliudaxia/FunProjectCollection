dcac27d2e0d3a0743d80c10221405d1252ab15f1
Yifei Liu <liu1456447215@gmail.com>
2025-03-11 15:50:24 +0800

    Add files via upload

--- cmd/迁移文件夹.bat
+++ cmd/迁移文件夹.bat
@@ -7,12 +7,26 @@ set /p folder2="请输入要迁移到的文件夹路径："
 for %%I in ("%folder1%") do set "foldername=%%~nI"
 echo %foldername%
 echo 正在复制文件夹...
-robocopy "%folder1%" "%folder2%\%foldername%" /MIR
+robocopy "%folder1%" "%folder2%\%foldername%" /E
 @echo 请检查复制文件夹是否出现了问题，如果没有问题按回车进入下一步
 pause
+
+:delete_folder
 echo 删除源文件夹... 
 rmdir /s /q "%folder1%"
-echo 建立链接...
-mklink /J "%folder1%" "%folder2%\%foldername%" 
-echo 完成啦！！
-pause
+if errorlevel 1 (
+    echo 删除源文件夹失败，请检查文件夹是否存在或是否被占用。
+) else (
+    echo 建立链接...
+    mklink /J "%folder1%" "%folder2%\%foldername%" 
+    echo 完成啦！！
+)
+
+:retry
+set /p retry="是否要重试删除源文件夹并建立连接？(y/n): "
+if /i "%retry%"=="y" (
+    goto delete_folder
+) else (
+    echo 操作结束。
+    pause
+)
